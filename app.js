5fc89a293571182bd19b9b3c04b6022328b3dbe8
Sam Hung <samhung0411@outlook.com>
2021-08-30 13:06:33 +1000

    added test-web-app

--- app.js
+++ app.js
@@ -4,12 +4,17 @@ const bodyParser = require("body-parser");
 const app = express();
 
 app.use(express.static("public"));
+app.use(express.static("web"));
 app.use(bodyParser.urlencoded());
 
 app.listen(process.env.PORT || 3000, function (){
     console.log("Server Started!");
 });
 
-app.get("*", function (req, res){
+app.get("/", function (req, res){
     res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/panel", function (req, res){
+    res.sendFile(__dirname + "/web/test.html");
 });
